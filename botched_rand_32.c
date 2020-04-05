06781fac1fc0803894329c582845bb25fbd506da
Lars Stoltenow <penma@penma.de>
2020-04-05 14:26:19 +0200

    split the prng to own source file, allowing multiple variants to be compiled

---
+++ botched_rand_32.c
@@ -0,0 +1,4 @@
+#define PLATFORM_LONG int32_t
+#define PLATFORM_UNSIGNED_LONG uint32_t
+#define FUNC_PREFIX i386_
+#include "botched_rand_base.c"
---
+++ host_flags.h
@@ -0,0 +1,10 @@
+#ifndef _HOST_FLAGS_H
+#define _HOST_FLAGS_H
+
+/*
+ * Define this macro if the code is to be compiled for older versions of OpenSSL
+ * that lack e.g. EVP_MD_CTX_new, otherwise leave it undefined
+ */
+#undef OLD_SSL
+
+#endif
